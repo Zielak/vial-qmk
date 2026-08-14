c6aa06957609f6f40667f18ffe532eaf61b628f6
Darek Greenly <Zielak@users.noreply.github.com>
2026-08-14 08:51:38 +0200

    alt etc

--- keyboards/bastardkb/scylla/keymaps/zielak/keymap.c
+++ keyboards/bastardkb/scylla/keymaps/zielak/keymap.c
@@ -36,24 +36,24 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         //-------------------------------------------------//-----------------------------------------------------------//
         KC_TAB, KC_Q, KC_W, KC_F, KC_P, KC_B,               KC_J, KC_L, KC_U, KC_Y, KC_SCLN, KC_DEL,
         //-------------------------------------------------//-----------------------------------------------------------//
-        KC_RALT, KC_A, KC_R, KC_S, KC_T, KC_G,              KC_M, KC_N, KC_E, KC_I, KC_O, KC_QUOT,
+        KC_LGUI, KC_A, KC_R, KC_S, KC_T, KC_G,              KC_M, KC_N, KC_E, KC_I, KC_O, KC_QUOT,
         //-------------------------------------------------//-----------------------------------------------------------//
-        KC_LSFT, KC_Z, KC_X, KC_C, KC_D, KC_V,               KC_K, KC_H, KC_COMM, KC_DOT, KC_SLSH, KC_BSLS,
+        KC_LSFT, KC_Z, KC_X, KC_C, KC_D, KC_V,              KC_K, KC_H, KC_COMM, KC_DOT, KC_SLSH, KC_BSLS,
         //-------------------------------------------------//-----------------------------------------------------------//
-                NAV_L, KC_LGUI, KC_SPC,                          KC_ENT , KC_RSFT, NUM_L,
-                        FNK_L,  KC_ENT,                          KC_RCTL, K_EMOJI
+                NAV_L, KC_RALT, KC_SPC,                          KC_ENT , KC_RSFT, NUM_L,
+                         FNK_L, KC_ENT,                          KC_RCTL, K_EMOJI
     ),
     [_QWERTY] = LAYOUT_split_4x6_5(
         KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5,               KC_6, KC_7, KC_8, KC_9, KC_0, KC_BSPC,
         //-------------------------------------------------//-----------------------------------------------------------//
         KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T,               KC_Y, KC_U, KC_I, KC_O, KC_P, KC_DEL,
         //-------------------------------------------------//-----------------------------------------------------------//
-        KC_RALT, KC_A, KC_S, KC_D, KC_F, KC_G,              KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT,
+        KC_LGUI, KC_A, KC_S, KC_D, KC_F, KC_G,              KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT,
         //-------------------------------------------------//-----------------------------------------------------------//
         KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B,              KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_BSLS,
         //-------------------------------------------------//-----------------------------------------------------------//
-                NAV_L, KC_LGUI, KC_SPC,                          KC_ENT , KC_RSFT, NUM_L,
-                        FNK_L,  KC_ENT,                          KC_RCTL, K_EMOJI
+                NAV_L, KC_RALT, KC_SPC,                          KC_ENT , KC_RSFT, NUM_L,
+                         FNK_L, KC_ENT,                          KC_RCTL, K_EMOJI
     ),
     [_NAV] = LAYOUT_split_4x6_5(
         _______, KC_EXLM, KC_AT  , KC_HASH, KC_DLR, KC_PERC,        KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______,
@@ -72,7 +72,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         //---------------------------------------------------------//-------------------------------------------------------//
         XXXXXXX, KC_GRV , KC_LCBR, KC_RCBR ,KC_PPLS ,XXXXXXX,       XXXXXXX, KC_P7, KC_P8, KC_P9, KC_COLN, _______,
         //---------------------------------------------------------//-------------------------------------------------------//
-        KC_AMPR, K_ARROW, KC_LPRN, KC_RPRN ,KC_PMNS ,XXXXXXX,       KC_EQL , KC_P4, KC_P5, KC_P6, KC_P0  , KC_UNDS,
+        KC_AMPR, K_ARROW, KC_LPRN, KC_RPRN ,KC_PMNS ,KC_EQL ,       XXXXXXX, KC_P4, KC_P5, KC_P6, KC_P0  , KC_UNDS,
         //---------------------------------------------------------//-------------------------------------------------------//
         KC_PIPE, KC_TILD, KC_LBRC, KC_RBRC ,KC_PSLS ,KC_PAST,       XXXXXXX, KC_P1, KC_P2, KC_P3, KC_PDOT, KC_BSLS,
         //---------------------------------------------------------//-------------------------------------------------------//
@@ -88,7 +88,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         //---------------------------------------------------------//-------------------------------------------------------//
         _______, KC_F9   ,KC_F10  ,KC_F11  ,KC_F12  ,XXXXXXX,       CG_SWAP ,KC_MPLY ,KC_MPRV ,KC_MNXT ,DT_PRNT, XXXXXXX,
         //---------------------------------------------------------//-------------------------------------------------------//
-                XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX ,KC_LCAP,
+                XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, KC_CAPS, KC_NUM, // TODO: see if those work on MAC
                          _ENTRY_, XXXXXXX,                   DF_QWER, DF_COLE
     ),
 
